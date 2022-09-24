2b3ccbdd8b0dcf398eb64bed367b5451f98b280c
Spencer Taira <spencermtaira@gmail.com>
2022-09-23 17:29:20 -0700

    added functionality so you cannot click multiple cards while waiting for wrong choices to flip back over

--- memory-game/demo/memory-game.js
+++ memory-game/demo/memory-game.js
@@ -44,7 +44,7 @@ function createCards(colors) {
   for (let color of colors) {
     // missing code here ...
     const card = document.createElement('div')
-    card.classList.add(`${color}`);
+    card.classList.add(`${color}`, 'card');
     card.addEventListener('click', cardClick)
     gameBoard.appendChild(card);
   }
@@ -60,33 +60,73 @@ function flipCard(card) {
 
 /** Flip a card face-down. */
 
-function unFlipCard(card1, card2) {
+function unFlipCard(card1, card2, count) {
   // ... you need to write this ...
   card1.style.backgroundColor = 'white';
   card2.style.backgroundColor = 'white';
+  count = 0;
 }
 
 /** Handle clicking on a card: this could be first-card or second-card. */
 
 function handleCardClick() {
   // ... you need to write this ...
+  let score = 0;
   let count = 0;
   let firstPick = '';
+  let newPick = '';
   return (e) => {
+    if (count === 2 && firstPick.style.backgroundColor === 'white' && newPick.style.backgroundColor === 'white') {
+      count = 0;
+    } else if (count === 2) return;
     if (e.target === firstPick) return;
     if (count === 0) {
       firstPick = flipCard(e.target);
       count++;
-    } else if (firstPick.style.backgroundColor === flipCard(e.target).style.backgroundColor) {
-      firstPick.removeEventListener('click', cardClick);
-      e.target.removeEventListener('click', cardClick);
-      console.log('match');
-      count = 0;
     } else {
-      console.log('no match');
-      setTimeout(unFlipCard, 1250, firstPick, e.target);
-      count = 0;
+      newPick = flipCard(e.target);
+      count++;
+      if (firstPick.style.backgroundColor === newPick.style.backgroundColor) {
+        firstPick.removeEventListener('click', cardClick);
+        newPick.removeEventListener('click', cardClick);
+        count = 0;
+        score++;
+      } else {
+        setTimeout(unFlipCard, 1250, firstPick, newPick);
+      }
     }
   }
 }
 
+// function checkCards(card1, card2) {
+//   if (card1.style.backgroundColor === card2.style.backgroundColor) {
+//     card1.removeEventListener('click', cardClick);
+//     card2.removeEventListener('click', cardClick);
+//   } else {
+//     unFlipCard(card1, card2);
+//   }
+// }
+
+// function handleCardClick() {
+//   // ... you need to write this ...
+//   let count = 0;
+//   let firstPick = '';
+//   return (e) => {
+//     console.log(count);
+//     if (count === 2) return;
+//     if (e.target === firstPick) return;
+//     if (count === 0) {
+//       firstPick = flipCard(e.target);
+//       count++;
+//     } else if (firstPick.style.backgroundColor === flipCard(e.target).style.backgroundColor) {
+//       firstPick.removeEventListener('click', cardClick);
+//       e.target.removeEventListener('click', cardClick);
+//       console.log('match');
+//       count = 0;
+//     } else {
+//       console.log('no match');
+//       count++;
+//       setTimeout(unFlipCard, 1250, firstPick, e.target, count);
+//     }
+//   }
+// }
